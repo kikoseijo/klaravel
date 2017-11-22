958251e7c8fecde28ab980163388b88bcc3b7f6d
Kiko Seijo <kiko@sunnyface.com>
2017-11-22 03:10:45 +0100

    Initial commit.

---
+++ src/CallsInteractions.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Ksoft\Klaravel;
+
+use Closure;
+use Illuminate\Support\Str;
+
+trait CallsInteractions
+{
+    /**
+     * Run an interaction method.
+     *
+     * @param  string  $interaction
+     * @param  array  $parameters
+     * @return mixed
+     */
+    public static function call($interaction, array $parameters = [])
+    {
+        return static::interact($interaction, $parameters);
+    }
+
+    /**
+     * Run an interaction method.
+     *
+     * @param  string  $interaction
+     * @param  array  $parameters
+     * @return mixed
+     */
+    public static function interact($interaction, array $parameters = [])
+    {
+        if (! Str::contains($interaction, '@')) {
+            $interaction = $interaction.'@handle';
+        }
+
+        list($class, $method) = explode('@', $interaction);
+
+        $base = class_basename($class);
+
+        return call_user_func_array([app($class), $method], $parameters);
+    }
+}

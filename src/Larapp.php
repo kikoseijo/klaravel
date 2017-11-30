4baae3279bc0445489e23be00b905003105deb6d
Kiko Seijo <kiko@sunnyface.com>
2017-11-30 01:19:09 +0100

    Downgrade Larapp Initial version for a more clear purpose.

--- src/Larapp.php
+++ src/Larapp.php
@@ -12,7 +12,7 @@ class Larapp
      * Make this yours.. by setting it up on bootstrap.
      * Larapp::$version = '1.2.3';
      */
-    public static $version = '0.0.2';
+    public static $version = '0.0.0';
 
 
     /**

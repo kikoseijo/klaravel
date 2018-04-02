7dcc9e3782a28b33bb30226eeb50ebf906b710f6
Kiko Seijo <kiko@sunnyface.com>
2018-04-02 21:13:01 +0200

    Fix capitalization error on script-

--- resources/layouts/app.blade.php
+++ resources/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Klaravel, by Sunnyface.com</title>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" integrity="sha384-3AB7yXWz4OeoZcPbieVW64vVXEwADiYyAEhwilzWsLw+9FgqpyjjStpPnpBO8o8S" crossorigin="anonymous">
-    @include('klaravel::_klara._parts._styles')
+    @include('klaravel::_kLara._parts._styles')
     @stack('stylesheets')
     <script>
         window.Larapp = {!! json_encode([
@@ -36,7 +36,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     @stack('scripts')
-    @include('klaravel::_klara._parts._scripts')
+    @include('klaravel::_kLara._parts._scripts')
     
 </body>
 </html>

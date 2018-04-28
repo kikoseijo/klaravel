5cba5099d4be27ebb0488798c145cacc5bcdc438
Kiko Seijo <kiko@sunnyface.com>
2018-04-28 03:51:54 +0200

    updated composer.

--- src/Traits/Models/HasLogs.php
+++ src/Traits/Models/HasLogs.php
@@ -6,13 +6,13 @@ use Spatie\Activitylog\ActivitylogServiceProvider;
 use Spatie\Activitylog\Traits\LogsActivity;
 
 use Illuminate\Database\Eloquent\Relations\MorphMany;
-/**
-* Trait HasLogs.
-*/
+
+
 trait HasLogs
 {
     use LogsActivity;
     protected static $recordEvents = ['deleted', 'created'];
+
     public function logs(): MorphMany
     {
         return $this->morphMany(

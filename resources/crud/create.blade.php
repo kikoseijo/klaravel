ce06c64f91947ac4124875d0650bcad2c3c7012a
Kiko Seijo <kiko@sunnyface.com>
2018-05-03 15:14:38 +0200

    Added hidden references to html layouts to find out what should be loading.

--- resources/crud/create.blade.php
+++ resources/crud/create.blade.php
@@ -5,6 +5,7 @@
     <div class="{{$crudWrapperClass}}">
         @card(['title' => 'New ' . title_case($model_name)])
             {!! Former::open_for_files()->route($model_name.'.store') !!}
+            <!-- {{$viewsBasePath.$model_name.'.form'}} -->
             @includeIf($viewsBasePath.$model_name.'.form')
             {!! Former::close() !!}
         @endcard

4e21a4e6ee8d0c66bab69ec499a34771643c284f
Kiko Seijo <kiko@sunnyface.com>
2018-05-12 11:44:04 +0200

    Prepare build.

--- stubs/config/ksoft.php
+++ stubs/config/ksoft.php
@@ -11,7 +11,7 @@
 */
 return [
 
-    'version' => '2.0.18',
+    'version' => '2.0.19',
     'models_path' => 'Models/', // defaults "Models/...."
     'backend_dashboard_route_name' => '',
     'klaravel_enabled' => true, // klaravel section route('kLara.index') + route('kLara.wiki')

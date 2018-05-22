1088503573ee7ee993aef9f7843bc71d0f873039
Kiko Seijo <kiko@sunnyface.com>
2018-05-22 13:10:42 +0200

    Fix.

--- stubs/config/ksoft.php
+++ stubs/config/ksoft.php
@@ -11,7 +11,7 @@
 */
 return [
 
-    'version' => '2.0.22',
+    'version' => '2.0.23',
     'models_path' => 'Models/', // defaults "Models/...."
     'backend_dashboard_route_name' => '',
     'klaravel_enabled' => true, // klaravel section route('kLara.index') + route('kLara.wiki')

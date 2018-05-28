b459d6595b7609789170032b9146bd3c7f63d2ad
Kiko Seijo <kiko@sunnyface.com>
2018-05-28 23:40:34 +0200

    'auth:api'

---
+++ resources/ui/tables/media_info.blade.php
@@ -0,0 +1,10 @@
+<!-- klaravel::ui.tables.media_info -->
+@if (isset($item))
+    {{$item->name}}<br />
+    <div class="badge badge-{{$item->size>600000?$item->size>1000000?'danger':'warning':'success'}} mr-3">
+        {{$item->human_readable_size}}
+    </div>
+    <div class="badge badge-info">{{$item->mime_type}}</div>
+@else
+    no <code>$item</code> defined
+@endif

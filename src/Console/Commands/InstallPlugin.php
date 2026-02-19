98b2a6728637110a6d18dc578a6adc88e2849527
Kiko Seijo (mmac) <kiko@sunnyface.com>
2026-02-20 00:18:44 +0100

    Fix deprecation.

--- src/Console/Commands/InstallPlugin.php
+++ src/Console/Commands/InstallPlugin.php
@@ -20,14 +20,14 @@ class InstallPlugin extends Command
             $this->info(shell_exec('sh '.$script));
         } else {
             $script = KLARAVEL_PATH ."/src/Console/plugins/composer_install.sh spatie/laravel-medialibrary:^6.0.0";
-            $res = exec("cd .. && sh ${script}");
+            $res = exec("cd .. && sh $script");
 
             // $command = 'cd .. && composer require spatie/laravel-medialibrary:^6.0.0';
             // $command = 'cd .. && pwd && composer require ' . $script_name['composer'] .' --no-scripts';
             // $res = exec($command);
             // $this->info(exec($composer_path . ' dump-autoload'));
             // logi($command);
-            logi($res);
+            // logi($res);
             return $res;
         }
 
